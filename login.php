58a71ad2bbbbf05043ee804ae1c41c61a8e0fb5d
Dhruv Rathwa <rathvadhruv3@gmail.com>
2025-01-17 21:02:33 +0530

    I change code

--- login.php
+++ login.php
@@ -1,16 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "car_rental";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+require_once 'connection.php'; // Include the connection file
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
